a2ef1bc71d7cd69ad33734279abfae3cc0ad8338
dmitriy <dmitri.zol@rambler.ru>
2022-11-23 19:10:53 +0300

    Basic clap::Parser application added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+// Simple program to add numbers
+#[derive(Parser)]
+struct Value {
+    // First number to add
+    #[clap(short = 'o', long)]
+    number_one: i32,
+    // First number to add
+    #[clap(short = 't', long)]
+    number_two: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let value = Value::parse();
+
+    let first_value = value.number_one;
+    let second_value = value.number_two;
+
+    let answer = first_value + second_value;
+
+    println!("The answer is: {answer}!");
 }
