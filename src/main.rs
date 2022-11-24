5583f39291c8ac6e3fe9166b5436c7278caaad1c
dmitriy <dmitri.zol@rambler.ru>
2022-11-24 15:52:26 +0300

    clap::Subcommand usage added

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-mod add;
-mod substract;
-mod multiply;
-mod divide;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(about, version, author)]
+struct Value {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { number_one: i32, number_two: i32 },
+    Sub { number_one: i32, number_two: i32 },
+    Mul { number_one: i32, number_two: i32 },
+    Div { number_one: i32, number_two: i32 },
+}
 
 fn main() {
-    add::add();
-    substract::substract();
-    multiply::multiply();
-    divide::divide();
+    let value = Value::parse();
+
+    match &value.command {
+        Commands::Add { number_one, number_two } => {
+           println!("Addition result is: {:?}", number_one + number_two)
+        },
+        Commands::Sub { number_one, number_two } => {
+           println!("Subtraction result is: {:?}", number_one - number_two)
+        },
+        Commands::Mul { number_one, number_two } => {
+           println!("Multiplication result is: {:?}", number_one * number_two)
+        },
+        Commands::Div { number_one, number_two } => {
+           println!("Division result is: {:?}", number_one / number_two)
+        },
+    }
 }
